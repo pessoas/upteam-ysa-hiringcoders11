42e09b77e5beb0f74f9dc870683910b34794f596
eltondouglas <eltondouglas@live.com>
2020-09-13 00:09:38 -0300

    Adicionado component chatbot

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-
 import React from "react";
-import Amplify from "aws-amplify";
-import {AmplifyChatbot as Chatbot} from "@aws-amplify/ui-react";
-import awsconfig from "./aws-exports";
+import Amplify from 'aws-amplify'
+import {AmplifyChatbot} from '@aws-amplify/ui-react';
+import awsconfig from './aws-exports';
 
 Amplify.configure(awsconfig);
 
+
 function App() {
 
-
-
   return (
-      <Chatbot
-        title="Ysa"
-        botName="ShoppingAssitant"
-        welcomeMessage="Bem vindo, diga oi para começar"
-        clearOnComplete={true}
-        conversationModeOn={false}
-        textEnabled={true}
-        voiceEnabled={true}
+    <>
+      <AmplifyChatbot
+        botName="Ysapilot_dev"
+        botTitle="YSA"
       />
+    </>
   );
 }
 
