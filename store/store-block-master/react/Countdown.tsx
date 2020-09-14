16e63be5f6cde9e0c2d7410efabbc57cb5b48273
Renato Pessoa <renatopessoaneto@gmail.com>
2020-09-13 23:57:39 -0300

    trocado a mensagem pela carinha do robo

--- store/store-block-master/react/Countdown.tsx
+++ store/store-block-master/react/Countdown.tsx
@@ -30,56 +30,13 @@ interface ApresentationProps {}
 }
 
 const Countdown: StorefrontFunctionComponent<ApresentationProps> = ({}) => {
+  const imgStyle = {
+      width: "130p",
+      height: "100px",
+  }
   return (
     <div>
-      <div id="container-apresentation" className="container-apresentation">
-        <div
-          id="container-apresentation-close"
-          className="container-apresentation-close"
-        >
-          <span>X</span>
-        </div>
-        <div className="box-message">
-          <div className="typewriter-text">
-            Seja bem vindo(a), estou muito feliz
-          </div>
-          <div className="typewriter-text">
-            que você veio! Meu nome é
-            <strong>
-              <abbr title="Your System Assistent">Ysa</abbr>
-            </strong>{" "}
-            e estou
-          </div>
-          <div className="typewriter-text">
-            aqui para auxiliar nas suas compras.
-          </div>
-          <div className="typewriter-text">
-            Quando precisar é só chamar, combinado?
-          </div>
-          <div className="typewriter-text">
-            Estarei no canto direito da sua tela.
-          </div>
-          <div className="typewriter-text">
-            Boas compras!!! &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
-            &nbsp &nbsp ;)
-          </div>
-        </div>
-
-        <div className="container-bot-and-message"></div>
-        <div className="imgBot"></div>
-      </div>
-
-      <div className="icon-chatbot-head">
-        <div className="icon-chatbot-ear">
-          <div></div>
-          <div></div>
-        </div>
-        <div className="icon-chatbot-eye">
-          <div className="eye"></div>
-          <div className="eye"></div>
-        </div>
-        <div className="icon-chatbot-mouth"></div>
-      </div>
+      <img style={imgStyle} src="https://upteam-ysa-chatbot.s3.amazonaws.com/headbot.png" />
     </div>
   );
 };
@@ -92,3 +49,57 @@ Countdown.schema = {
 };
 
 export default Countdown;
+
+
+/*
+<div>
+  <div id="container-apresentation" className="container-apresentation">
+    <div
+      id="container-apresentation-close"
+      className="container-apresentation-close"
+    >
+      <span>X</span>
+    </div>
+    <div className="box-message">
+      <div className="typewriter-text">
+        Seja bem vindo(a), estou muito feliz
+      </div>
+      <div className="typewriter-text">
+        que você veio! Meu nome é
+        <strong>
+          <abbr title="Your System Assistent">Ysa</abbr>
+        </strong>{" "}
+        e estou
+      </div>
+      <div className="typewriter-text">
+        aqui para auxiliar nas suas compras.
+      </div>
+      <div className="typewriter-text">
+        Quando precisar é só chamar, combinado?
+      </div>
+      <div className="typewriter-text">
+        Estarei no canto direito da sua tela.
+      </div>
+      <div className="typewriter-text">
+        Boas compras!!! &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
+        &nbsp &nbsp ;)
+      </div>
+    </div>
+
+    <div className="container-bot-and-message"></div>
+    <div className="imgBot"></div>
+  </div>
+
+  <div className="icon-chatbot-head">
+    <div className="icon-chatbot-ear">
+      <div></div>
+      <div></div>
+    </div>
+    <div className="icon-chatbot-eye">
+      <div className="eye"></div>
+      <div className="eye"></div>
+    </div>
+    <div className="icon-chatbot-mouth"></div>
+  </div>
+</div>
+*/
