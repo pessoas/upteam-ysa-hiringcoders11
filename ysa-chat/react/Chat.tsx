482863d840ff42e26979fb858faa51886c8ca097
esdrasli <42820970+esdrasli@users.noreply.github.com>
2020-09-22 17:49:03 -0300

    checkout e home

--- ysa-chat/react/Chat.tsx
+++ ysa-chat/react/Chat.tsx
@@ -10,14 +10,14 @@ declare global {
 function Chat(){
 
     useEffect(() => {
-        (function (_d: any, m: any) {
-            var kommunicateSettings = { "appId": "b9e080f270d9861066abce62b78b8ead", "popupWidget": true, "automaticChatOpenOnNavigation": true };
-            var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-            s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-            var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-            window.kommunicate = m; m._globals = kommunicateSettings;
-        })(document, window.kommunicate || {});
-    });
+        (function(d, m){
+        var kommunicateSettings = 
+            {"appId":"b9e080f270d9861066abce62b78b8ead","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
 
     return (
         <>
